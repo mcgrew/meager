04b3f4fb337946dea53595d11d38c2d4dcd6d367
Thomas McGrew <tjmcgrew@gmail.com>
2009-05-27 11:28:18 -0400

    Added 404 error support for files which are not existent.

--- base.php
+++ base.php
@@ -8,8 +8,11 @@ include( 'configuration.php' );
 include( 'modules.php' );
 
 $modules->register( 'content',  "../$current_page" );
+if ( !file_exists( $current_page ))
+	$modules->register( 'content', $modules->get_filename( 'STATUS_404' ));
 
+ob_start( );
 include ( 'templates/'.get_template( ).'/index.php' );
-
+ob_flush( );
 
 ?>
--- modules/STATUS/404.php
+++ modules/STATUS/404.php
@@ -1,6 +1,9 @@
 <?php
 
-header( 'HTTP/1.0 404 Not Found' )
-echo "<h1>404 File not found</h1>"
+header( 'HTTP/1.0 404 Not Found' );
+echo "<h1>Not Found</h1>";
+echo "<p>The requested URL ".current_page( )." was not found on this server.</p>";
+echo "<p><a href='.'>Back to ".$config[ 'sitename' ]." home</p>";
+
 
 ?>
