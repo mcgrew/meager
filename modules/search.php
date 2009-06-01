a9bc7ac54e3b19b32268d65a2f4748de43268646
Thomas McGrew <tjmcgrew@gmail.com>
2009-06-01 12:32:41 -0400

    Fixed search module to use google site specific search

--- modules/search.php
+++ modules/search.php
@@ -19,23 +19,10 @@ $focusAction     = $FOCUS_ACTION[  $this->get_opt( 'focus_action', 0)  ];
 $defaultValue    = $this->get_opt( 'default_value', "" );
 
 echo "
-<script type='text/javascript'><!--
-
-function searchEcoliWebsearch(frm)
-{
-	var myVal = 'index.php?option=com_wrapper%26Itemid=250%26query=';
-	var query = frm.query.value;
-	var urlForward = myVal + query;
-	var url = 'logSearches.php?query=' + query + '&url_forward=' + urlForward;
-	window.open(url, '_self');
-	return false;
-}
-
---></script>
 <div class='search_box module$moduleclass_sfx' id='$containerID'>
 
-<form name='BannerSearchBoxForm' id='search_box_form' method='get' onsubmit='return searchEcoliWebsearch(this);'>
-        <input class='search_box_text' name='query' value='$defaultValue' id='headerSearchInput' onfocus='$focusAction' type='text' style='height:18px;' />
+<form name='BannerSearchBoxForm' id='search_box_form' method='get' action='http://www.google.com/search' onsubmit='this.q.value = \"site:".$_SERVER['SERVER_NAME']." \"+this.q.value;'>
+        <input class='search_box_text' name='q' value='$defaultValue' id='headerSearchInput' onfocus='$focusAction' type='text' style='height:18px;' />
         <input type='submit' value='SEARCH' alt='Submit' id='headerSearchButton' style='vertical-align:top;' />
         </a>
 </form>
